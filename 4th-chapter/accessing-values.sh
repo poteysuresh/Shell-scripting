ab09860e3379f16a542b1ff4d250670e4f3bed45
suri <suri@linux.com>
2024-04-22 19:36:16 +0530

    variale substitution

---
+++ 4th-chapter/accessing-values.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+a=10
+b=20
+course=LINUX
+practice="suri is is learning"
+
+
+echo "value of a ${a}"
+echo "value of a ${b}"
+echo "value of a ${course}"
+echo "value of a ${practice}"
